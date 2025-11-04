9c6caaa580e8b40962e87e75ed65280ad4eef019
Hamzalateef <dev.hamza.lateef@gmail.com>
2025-11-03 16:48:08 -0800

    first commit

---
+++ jobusers.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function JobUsers() {
+  
+
+  return (
+    <div className='w-full bg-green-500 flex-row justify-between'>
+        <div ><h3>Users List</h3></div>
+
+    </div>
+  );
+}
